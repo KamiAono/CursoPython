336b8772b3267f7fdd0bd0629fdbf7b2892a485e
Raimundo Junior <61881055+KazumaShachou@users.noreply.github.com>
2020-09-08 15:08:17 -0400

    Update and rename Fazendo_desconto.py to Opin_sobrenomes.py
    Pergunta o nome do usuário o elogia...ou não!

--- Opin_sobrenomes.py
+++ Opin_sobrenomes.py
@@ -1,11 +1,11 @@
 nome = str(input('Qual o seu nome?')).upper().strip()
 if nome == 'RAI':
-    print ('\033[31mVocê é muito lindo!')
+    print ('\033[31mVocê tem um nome muito lindo!')
 elif nome == 'PEDRO' or nome == 'MARIA' or nome == 'LUCAS' or nome == 'JOÃO':
-    print('Seu nome é bm popular no Brasil!')
+    print('Seu nome é bem popular no Brasil!')
 elif nome in 'LUANA, KIANA, YUUKA, PATRICIA':
     print('Belo nome')
 else:
-    print('Você é muito normal! Eca')
+    print('Você é muito normal!')
 print('Bom dia {}'.format(nome))
 #Isso é uma estrutura condicional aninhada, uma estrutura dentro da outra

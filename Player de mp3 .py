795a2dc7cb61fdbdd2cd4630990b86efde7d0804
Raimundo Junior <61881055+KazumaShachou@users.noreply.github.com>
2020-09-08 14:55:03 -0400

    Update and rename Exercicio 21 Aula 8.py to Player de mp3 .py

--- Player de mp3 .py
+++ Player de mp3 .py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.mixer.init()
 pygame.init()
-pygame.mixer.music.load('konosubaedremix.mp3')
+pygame.mixer.music.load('copie e cole uma musica em .mp3 aqui')
 pygame.mixer.music.play()
 pygame.event.wait()
 
